cbdde331fc5fd6333d24a9fe25ba1669a40b7de6
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-07-24 18:42:52 +0300

    Hello

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints array with commas and spaces between elements
+ * @a: array to be printed
+ * @n: size of array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++;)
+	{
+		if (i == (n - 1))
+			printf("%d", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+
+}
