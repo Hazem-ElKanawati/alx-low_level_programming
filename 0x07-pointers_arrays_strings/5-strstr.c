cd3e68f2c95f034016a4d446c1a9657252efc04b
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-07-31 16:08:36 +0300

    Hello

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - searches for a substring in a string
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to the first char of the substring or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j = 1, m, n;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			while (haystack[i + j] == needle[j])
+			{
+				j++;
+			}
+			if (needle[j] == '\0')
+				return (&haystack[i]);
+		}
+
+	}
+	return (NULL);
+}
