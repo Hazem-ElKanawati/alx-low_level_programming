3f9a7aaf758fe5faaf55407f1a68698de4947fad
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-07-31 15:47:19 +0300

    Hello

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locate the first match of characters from 2 arrays
+ * @s: the 1st array
+ * @accept: the 2nd array
+ * Return: NULL if nothing matches or a pointer to the first matching char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+}
