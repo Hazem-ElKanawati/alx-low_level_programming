e7bb4a1fec405f62e70c05f1acd931be8fd3300f
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-02 15:01:57 +0300

    Hello

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the first element of the string
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s != '\0')
+		_puts_recursion(++s);
+	else
+		return;
+}
