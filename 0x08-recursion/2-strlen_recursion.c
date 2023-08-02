1398d611c79812cf43b23a163acc2b1b5adde85f
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-02 15:47:37 +0300

    Hello

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculate the number of chars in a string
+ * @s: pointer to first char in the string
+ * Return: number of chars in the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}	
