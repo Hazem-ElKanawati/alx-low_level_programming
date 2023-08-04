c230b165fc8481eb3ef1e7084225008ed812fc9c
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-04 17:19:38 +0300

    Hello

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main- Prints the name of the file
+ * Return: always 0
+ * @argc: arguments count
+ * @argv: array of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", *argv);
+}
