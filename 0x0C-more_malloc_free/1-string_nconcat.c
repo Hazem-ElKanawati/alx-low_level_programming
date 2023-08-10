d9e0d4a1f9ae74d5daf3fcb909162a174ac38adf
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-10 13:05:56 +0300

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,45 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: 1st string
- * @s2: 2nd string
- * @n: the first n bytes from the 2nd string to be copied
- * Return: Pointer to the New allocated string (Success) NULL (Error)
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, size1 = 0;
-	char *p;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1[size1] != 0)
-		size1++;
-	p = malloc(sizeof(char) * (size1 + n + 1));
-	if (p == NULL)
-		exit(1);
-	for (i = 0; i < size1; i++)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
 	{
-		p[i] = s1[i];
-	}
-	for (j = 0; j <= n; j++)
-	{
-		p[i] = s2[j];
+		s[i] = s1[i];
 		i++;
 	}
-	p[i] = '\0';
-	return (p);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
