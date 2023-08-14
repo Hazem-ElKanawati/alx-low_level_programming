4e20d645e1aac969fb2c820f1631d0c19246e930
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-14 23:09:53 +0300

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,35 @@
 #include "dog.h"
 
 /**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -27,8 +56,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free (p);
 		return (NULL);
 	}
-	p->name = name;
-	p->owner = owner;
+	_strcpy(p->name, name);
+	_strcpy(p->owner, owner);
 	p->age = age;
 	return (p);
 }
