d6db33f4ab002f91efe7a5596abfe99499f75857
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-16 22:03:53 +0300

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,14 +5,14 @@
  * array_iterator - acts a function on each element of an array
  * @array: The array
  * @size: The size of the array
- * @aciton: The function to be implemented
+ * @action: The function to be implemented
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if(array == NULL || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
