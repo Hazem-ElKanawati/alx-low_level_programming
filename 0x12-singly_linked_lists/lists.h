374762ec4a859a24d5d4dc4a79c54484b0fff534
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-24 11:37:57 +0300

    Task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,20 @@
 #ifndef LISTS_H
-#define LISTS_H 
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct list_s - linked list
+ * @str: string
+ * @len: len of the string
+ * @next: pointer to next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 #endif
