ccd8514e1c8cf571a2ed82de99de7c419982d1d4
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-08-24 11:32:06 +0300

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists .h"
+
+/**
+ * print_list - prints all elemetns in a list
+ * @h: pointer to the list
+ * Return: No. of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
