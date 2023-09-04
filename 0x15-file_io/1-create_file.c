a72165aee566c8a785357c7b02101c2c6fdfeb30
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-09-04 23:39:18 +0300

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,14 +9,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, i;
+	int o, w, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
-
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, i);
 	if (o == -1 || w == -1)
