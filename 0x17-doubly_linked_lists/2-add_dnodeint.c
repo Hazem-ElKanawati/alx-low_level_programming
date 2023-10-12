bca99622ad3b95ba4af5c457056bb046f81303e3
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-10-12 19:09:47 +0300

    Task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node to the head of the list
+ * @head: head of the list
+ * @n: element
+ * Return: ptr to the new node (Success) NULL (Failed)
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *ptr = malloc(sizeof(dlistint_t));
+
+	while ((*head)->prev != NULL)
+		h = h->prev;
+	if (ptr == NULL)
+		return (NULL);
+	if (*head == NULL)
+		return (NULL);
+	(*head)->prev = ptr;
+	ptr->next = (*head);
+	ptr->n = n;
+	ptr->prev = NULL;
+	return (ptr);
+}
