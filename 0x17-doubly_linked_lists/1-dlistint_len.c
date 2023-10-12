a677d59d7c91cd19617468cf2fdd8f92341043e3
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-10-12 18:57:31 +0300

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns No. of elements
+ * @h: A node in the list
+ * Return: No. of elements in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
