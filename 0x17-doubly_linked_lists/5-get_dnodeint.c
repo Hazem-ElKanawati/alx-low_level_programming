9dedb795d2ad7d1aa48fc5b71ac9e83d4c4c449e
Hazem ElKanawati <hazemelkanawati@gmail.com>
2023-10-12 21:29:59 +0300

    Task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a list
+ * @head: head of the list
+ * @index: n
+ * Return: the nth node (Success) NULL (Fail)
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head == NULL)
+		return (NULL);
+	while (head->prev != NULL)
+		head = head->prev;
+	for (i = 0; i < index; i++)
+	{
+		if (head->next != NULL)
+			head = head->next;
+		else
+			return (NULL);
+	}
+	return (head);
+}
