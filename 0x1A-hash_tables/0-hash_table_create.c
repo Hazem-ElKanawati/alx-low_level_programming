873f780fd3cb14e2fda1762ea6504a7d3e79475c
Hazem ElKanawati <hazemelkanawati@gmail.com>
2024-04-14 23:28:25 +0200

    Task 0 -2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t x;
-	int i = 0;
+	unsigned long int i = 0;
 	hash_table_t *y;
 
 	x.array = malloc(sizeof(hash_node_t *) * size);
